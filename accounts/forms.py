ee15173db4b7eb51489dd0ec29db225b0fb458d8
ninanolets <marinaa.noleto@gmail.com>
2020-11-27 07:14:55 +0100

    Fix image bug

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import Profile
 
+from imagekit.forms import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.TextInput(None)
@@ -25,7 +28,14 @@ class UserUpdateForm(forms.ModelForm):
         fields = ['username', 'first_name', 'last_name', 'email']
 
 class ProfileUpdateForm(forms.ModelForm):
+    image = ProcessedImageField(spec_id='accounts:profile:image',
+                                processors=[ResizeToFill(300, 300)],
+                                format='JPEG',
+                                options={'quality': 60})
+
     class Meta:
         model = Profile
         fields = ['image']
 
+
+    
--- cocktail/settings.py
+++ cocktail/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
     'django.contrib.humanize',
     'crispy_forms',
+    'imagekit',
 
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
